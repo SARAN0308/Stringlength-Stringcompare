23606e5471ee9d2868382f3a943606c89cb7e2a7
Saran_Techie03 <79828722+SARAN0308@users.noreply.github.com>
2021-03-11 06:48:31 -0800

    Add files via upload

---
+++ stringlength.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+public class Stringlength {
+	public static void main(String[] args) {
+int i=0;
+String str;
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the string");
+str=sc.nextLine();
+int n=str.length();
+System.out.println("length of the strig"+n);
+	}
+}
